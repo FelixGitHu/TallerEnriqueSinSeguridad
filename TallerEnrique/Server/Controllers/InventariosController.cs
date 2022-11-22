91bb7a6365a1db42e053817b415d5646af34f191
Felix Narvaez <felixgithup12@gmail.com>
2022-11-22 10:39:43 -0600

    Visualizacion del inventario (Carga de la lista de los productos en el inventario)

---
+++ TallerEnrique/Server/Controllers/InventariosController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TallerEnrique.Shared.Entidades;
+
+namespace TallerEnrique.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class InventariosController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public InventariosController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet("Cargar")]
+        public async Task<ActionResult<List<Inventario>>> Get()
+        {
+            return await context.Inventarios.Include("Articulo").ToListAsync();
+        }
+    }
+}
