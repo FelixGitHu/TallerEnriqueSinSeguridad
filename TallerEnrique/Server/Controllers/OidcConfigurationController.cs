629c03932560b75584743878750d3335d52ac3a1
Felix Narvaez <felixgithup12@gmail.com>
2023-01-09 15:51:36 -0600

    Identity server 4 no funciona (deberia de funcionar bien)

---
+++ TallerEnrique/Server/Controllers/OidcConfigurationController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TallerEnrique.Server.Controllers
+{///Trae la configuracion del cliente que se esta intentando conectar a nuestra instancia de Identity server
+    public class OidcConfigurationController : Controller
+    {
+        private readonly ILogger<OidcConfigurationController> _logger;
+
+        public OidcConfigurationController(IClientRequestParametersProvider clientRequestParametersProvider, 
+            ILogger<OidcConfigurationController> logger)
+        {
+            ClientRequestParametersProvider = clientRequestParametersProvider;
+            _logger = logger;
+        }
+
+        public IClientRequestParametersProvider ClientRequestParametersProvider { get; }
+
+        [HttpGet("_configuration/{clientId}")]
+        public IActionResult GetClientRequestParameters([FromRoute]string clientId)
+        {
+            var parameters = ClientRequestParametersProvider.GetClientParameters(HttpContext, clientId);
+            return Ok(parameters);
+        }
+    }
+}
